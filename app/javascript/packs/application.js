32f74a621f3df0f559233e169b0943754da23646
michelin_m <mm19022000@gmail.com>
2021-02-16 10:21:41 +0200

    Moved JS files to javascript/packs directory

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require("packs/carriages")
+require("packs/railway_stations")
+require("packs/trains")
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -5,7 +5,11 @@ class RailwayStation < ApplicationRecord
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
 
-  scope :asc, -> { order('railway_stations_routes.station_position') }
+  scope :asc, lambda {
+    select('railway_stations.*, railway_stations_routes.station_position')
+      .joins(:railway_stations_routes)
+      .order('railway_stations_routes.station_position')
+  }
 
   def update_position(route, position)
     station_route = station_route(route)
